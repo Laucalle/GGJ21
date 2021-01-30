4228702f64fcd089aad64553e0923ee661b4e0cc
Laura Calle <laucalle09@gmail.com>
2021-01-30 12:45:02 +0100

    some initial text

---
+++ OniricoAndMe/Assets/Sripts/Line.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public enum Character{ Sirio, Deneb};
+    public string textLine;
+    public Character character;
+}
---
+++ OniricoAndMe/Assets/Sripts/Passage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Passage : MonoBehaviour
+{
+    public List<Line> nonInteractiveLines = new List<Line>();
+    int nextDisplayed = 0;
+    public RectTransform masterPanel;
+    public GameObject textPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown){
+            // instanciamos la siguiente linea de dialogo si la hay, si no la opcion.
+            if (nextDisplayed < nonInteractiveLines.Count - 1)
+            {
+                Text texto = Instantiate(textPrefab, masterPanel).GetComponent<Text>();
+                // later on animation migth be here or in the prefab itself
+                texto.text = nonInteractiveLines[nextDisplayed].textLine;
+                // TODO define proper colors
+                texto.color = nonInteractiveLines[nextDisplayed].character == Line.Character.Deneb ? Color.red : Color.cyan;
+
+                nextDisplayed++;
+            }
+            else {
+                // instanciamos la opcion 
+                // si no ha sido instanciada
+                // else no hago nada
+            }
+        }
+    }
+}
