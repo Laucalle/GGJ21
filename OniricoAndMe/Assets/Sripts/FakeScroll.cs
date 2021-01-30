9a243490c77b97551e042d373f347d94dfca8b87
Laura Calle <laucalle09@gmail.com>
2021-01-30 22:03:40 +0100

    scroll pocho que destruye texto

---
+++ OniricoAndMe/Assets/Sripts/FakeScroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeScroll : MonoBehaviour
+{
+    public int maxChildren = 4;
+
+    private void OnTransformChildrenChanged()
+    {
+        Debug.Log("A CHILD WA ADDED");
+        if (transform.childCount > maxChildren)
+        {
+            Destroy(gameObject.transform.GetChild(0).gameObject);
+        }
+    }
+}
